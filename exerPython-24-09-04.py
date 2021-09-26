20edcfaf91669c8ce7c1bf8cc2c621061e5c39c2
Leonardo <oleoalonso@gmail.com>
2021-09-26 16:49:01 -0300

    Atividade 04

---
+++ exerPython-24-09-04.py
@@ -0,0 +1,12 @@
+# Leonardo L. Alonso Carriço
+# leonardo@carrico.com.br
+
+# Exercício Prático Python [24/09] 04
+
+###################################################################
+# 4) Faça um programa, utilizando while, que mostre na tela os números de 0 a 100.
+
+x = 0
+while x < 101:
+  print(x)
+  x += 1
